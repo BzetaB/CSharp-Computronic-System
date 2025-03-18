f1f569f8994e9b921ac30ae8defdb8d2afd21347
bzetab <zevallosbenjamin100@gmail.com>
2025-03-18 13:37:03 -0500

    Recuperado README.md

--- UI/Form1.cs
+++ UI/Form1.cs
@@ -5,16 +5,13 @@ namespace UI
 {
     public partial class Form1 : Form
     {
-        private readonly IMarcaService _marcaService;
-        public Form1(IMarcaService marcaService)
+        public Form1()
         {
             InitializeComponent();
-            _marcaService = marcaService;
         }
 
-        private async void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            var lista = await _marcaService.listMarcaProducts();
         }
     }
 }
